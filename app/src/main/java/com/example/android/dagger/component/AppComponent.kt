672c55d6079f7ccb89677c2eeccc8234c0942ea8
eddyjyliu <eddyjyliu@tencent.com>
2020-06-20 12:10:41 +0800

    step>>>to the step 8 https://codelabs.developers.google.com/codelabs/android-dagger/#6

--- app/src/main/java/com/example/android/dagger/component/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/component/AppComponent.kt
@@ -1,13 +1,32 @@
 package com.example.android.dagger.component
 
+import android.content.Context
+import com.example.android.dagger.login.LoginActivity
+import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
 import dagger.Component
 
-@Component
+@Component(modules = [StorageModule::class])
 interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context) : AppComponent
+    }
 
     /**
      * the RegistrationActivity inject function
      */
     fun inject(registrationActivity: RegistrationActivity)
+
+    /**
+     * the LoginActivity inject function
+     */
+    fun inject(loginActivity: LoginActivity)
+
+    /**
+     * the MainActivity inject function
+     */
+    fun inject(mainActivity: MainActivity)
 }
